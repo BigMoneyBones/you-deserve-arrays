fed666b8624732678758b326ff081f2b76ddcd03
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-15 09:03:04 -0600

    updated with psuedo code

--- firstItemIsNumber.js
+++ firstItemIsNumber.js
@@ -1,11 +1,16 @@
 const prompt = require('prompt-sync')({sigint: true});
 
+//Sets prompt type to be recognized as an array, not a 'string'
 let userArray = JSON.parse(prompt("Enter an array: "));
 
+//Declares a variable for the first item in the array.
 let firstItem = userArray[0];
+//Declares a boolean variable to be used.
 let notANum = false;
 
+//If the first item in the array is a number, log true, otherwise it will print false to screen.
 if (typeof firstItem === 'number') {
     notANum = true;
 }
-console.log(notANum);
+//Logs false to screen if user input for the first item is not a number.
+console.log("The first item in this array is a number: " + notANum);
--- secondCharOfThirdString.js
+++ secondCharOfThirdString.js
@@ -1,12 +1,19 @@
 const prompt = require('prompt-sync')({sigint: true});
 
+//Lets prompt know its looking for an array and not a 'string'
 let userArray = JSON.parse(prompt("Enter an array: "));
 
+//Declare a variable for the item at index[3] in the array.
+//The number '2' is used because count starts at 0. ex: 0,1,2 -- '2' is the 3rd index.
 let item3 = userArray[2];
+
+//Declare a variable for the 2nd character in the 3rd item.
+//The number '1' is used because count starts at 0. ex: 0,1,2 -- '1' is the 2nd char index.
 let char2 = item3[1];
 
-if(typeof item3 === 'string') {
+
+if(typeof item3 === 'string') { //If the third item is a string and not a number, it will log out the statement.
     console.log("The second character of the third item in this array is: " + char2);
-} else {
+} else { //If the third item is not a string, an error message will appear.
     console.log("ERROR");
 }
