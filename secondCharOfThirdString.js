514e9bc6b24234ff617779f70c204112ca749414
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-15 08:51:27 -0600

    update

--- secondCharOfThirdString.js
+++ secondCharOfThirdString.js
@@ -1,2 +1,12 @@
 const prompt = require('prompt-sync')({sigint: true});
 
+let userArray = JSON.parse(prompt("Enter an array: "));
+
+let item3 = userArray[2];
+let char2 = item3[1];
+
+if(typeof item3 === 'string') {
+    console.log("The second character of the third item in this array is: " + char2);
+} else {
+    console.log("ERROR");
+}
